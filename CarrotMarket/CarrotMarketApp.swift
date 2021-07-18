0a1f89b95a7c17bf7603d9b3c56105a8cb54caaa
urijan44 <teamsva360@gmail.com>
2021-07-18 20:04:47 +0900

    Initial Commit

---
+++ CarrotMarket/CarrotMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrotMarketApp.swift
+//  CarrotMarket
+//
+//  Created by hoseung Lee on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct CarrotMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
